c1d352099fc3e6cb12fbe70277fde14ba766c5aa
Liam Cammpbell <liam22campbell@gmail.com>
2022-09-14 11:55:05 -0700

    Test out action creators and constants
    Co-authored-by: bpgoodrow <bpgoodrow@users.noreply.github.com>

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,4 +1,5 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer';
+import * as c from '../../actions/ActionTypes';
 
 describe("formVisibleReducer", () => {
 
@@ -7,7 +8,11 @@ describe("formVisibleReducer", () => {
   });
 
   test('Should toggle form visibility state to true', () => {
-    expect(formVisibleReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
+    expect(formVisibleReducer(false, 'TOGGLE_FORM')).toEqual(true);
+  });
+  
+  test('Should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(false, c.TOGGLE_FORM)).toEqual(true);
   });
 
 });
